5c5aba893fb037c1e778a5c9ce3dd10f6e6a9b10
Dimas Aristyo Rahadian <84317322+DeAristyo@users.noreply.github.com>
2021-05-22 15:33:43 +0700

    Create KotakBintang.java

---
+++ KotakBintang.java
@@ -0,0 +1,29 @@
+public class KotakBintang {
+    public static void main(String[] args){
+
+        int x=5;
+        System.out.println(" --- panjang --- ");
+
+        for(int i=1;i<=x;i++){
+            for(int j=1;j<=x;j++){
+                if(i==1||i==3||i==x||j==1||j==x){
+                    System.out.print("*   ");
+                } else {
+                    System.out.print("=   ");
+                }
+            }
+            System.out.println();
+        }
+        
+    }
+    
+}
+
+  
+  //output
+//   --- panjang --- 
+//  *   *   *   *   *   
+//  *   =   =   =   *   
+//  *   *   *   *   *   
+//  *   =   =   =   *    
+//  *   *   *   *   *   
