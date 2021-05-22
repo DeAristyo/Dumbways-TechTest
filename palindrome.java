5a853ee8be1dd16d3a1e72865ec8c3a959e95269
Dimas Aristyo Rahadian <84317322+DeAristyo@users.noreply.github.com>
2021-05-22 12:28:03 +0700

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -1,20 +1,20 @@
-public class Palindrom {
-        public static void main(String[] args) {
-            int integer = 36163;
-            int temp = integer;
-            int remainder;
-            int reverse = 0;
-            while (integer > 0){
-                remainder = integer%10;
-                reverse = reverse * 10 + remainder;
-            integer=integer /10;
-        }
-    
-        if (temp==reverse)
-            System.out.println("True");
-        else
-            System.out.println("False");
-        }
+public class Palindrome {
+    public static void main(String[] args) {
+        int angka = 36163;
+        int temp = angka;
+        int remainder;
+        int reverse = 0;
+        while (angka > 0){
+            remainder = angka%10;
+            reverse = reverse * 10 + remainder;
+        angka=angka /10;
+    }
+
+    if (temp==reverse)
+        System.out.println("True");
+    else
+        System.out.println("False");
+    }
 }
 
 // Output : True
