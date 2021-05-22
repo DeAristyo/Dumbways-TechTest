4befa81960e6bf02ec14aa1025d58db3fa67fbb3
Dimas Aristyo Rahadian <84317322+DeAristyo@users.noreply.github.com>
2021-05-22 12:18:32 +0700

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,20 @@
+public class Palindrome {
+        public static void main(String[] args) {
+            int integer = 36163;
+            int temp = integer;
+            int remainder;
+            int reverse = 0;
+            while (integer > 0){
+                remainder = integer%10;
+                reverse = reverse * 10 + remainder;
+            integer=integer /10;
+        }
+    
+        if (temp==reverse)
+            System.out.println("True");
+        else
+            System.out.println("False");
+        }
+}
+
+// Output : True
